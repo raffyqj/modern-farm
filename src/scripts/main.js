64c63253f6dedd52072104a503ad4ecca2b0ec5a
Raffaela Quilodran <raffyqj@gmail.com>
2023-01-22 16:57:41 -0500

    finish adding imports

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,8 +5,10 @@
 
 const {createAsparagus} = require("./seeds/asparagus.js")
 const {createSoybean} = require("./seeds/soybeans.js")
-
-
+const {createCorn} = require("./seeds/corn.js")
+const {createSunflower} = require("./seeds/sunflower.js")
+const {createWheat} = require("./seeds/wheat.js")
+const {createPotato} = require("./seeds/potato.js")
 
 
 console.log(createSoybean())
